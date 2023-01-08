5c44e7b8d91d47cd12502f685445884960728855
Prasad Surase <prasad.surase@coupa.com>
2023-01-08 22:04:19 +0530

    Added basic routes

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/prasadsurase/go-bookstore/pkg/controllers/booksController"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/books", booksController.Books).Methods("GET")
+	router.HandleFunc("/book", booksController.Book).Methods("GET")
+	router.HandleFunc("/books", booksController.CreateBook).Methods("POST")
+	router.HandleFunc("/books/{id}", booksController.UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", booksController.DeleteBook).Methods("DELETE")
+}
