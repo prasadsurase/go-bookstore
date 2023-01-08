9ec4654d73414aa6a57b469b1380b86f53946c77
Prasad Surase <prasad.surase@coupa.com>
2023-01-08 22:04:42 +0530

    Added placeholder methods for books controller

---
+++ pkg/controllers/books-controller.go
@@ -0,0 +1,23 @@
+package booksController
+
+import "net/http"
+
+func Books(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Book(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
